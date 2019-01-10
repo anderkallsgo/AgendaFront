bcfe7bf404d229a374e22778d061d85dd5d538a1
adfigueiro <adfigueiro@fazenda.ms.gov.br>
2019-01-10 09:32:47 -0300

    Criado os components Header e Footer e Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { CadastrarComponent } from './agendamento/cadastrar/cadastrar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    CadastrarComponent
   ],
   imports: [
     BrowserModule,
